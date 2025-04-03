c21bcbf899c9de45d2f869869d57953565ad8611
unknown <alexiprangegoncalves.trabalho@gmail.com>
2025-04-03 09:33:58 -0300

    v1

--- atividade-sala/index.ts
+++ atividade-sala/index.ts
@@ -2,11 +2,11 @@ import { Livro } from "./Livro";
 import { Revista } from "./Revista";
 import { Artigo } from "./Artigo";
 
-const a = new Livro("hamlet", "Shakespeare", 203);
-a.exibirDetalhe;
+const A = new Livro("hamlet", "Shakespeare", 203);
+A.exibirDetalhe();
 
-const b = new Revista("Veja", "Indefinido", 2);
-b.exibirDetalhe;
+const B = new Revista("Veja", "Indefinido", 2);
+B.exibirDetalhe();
 
-const c = new Artigo("Crise do cuidado" , "Nancy Fraser", new Date(2019,2,6));
-c.exibirDetalhe;
+const C = new Artigo("Crise do cuidado" , "Nancy Fraser", new Date(2019,2,6));
+C.exibirDetalhe();
